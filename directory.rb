4403c2d9c524918d7126ccf61d2187f174c7257f
Charles Kettell <charleskettell@hotmail.co.uk>
2019-06-19 06:47:06 +0000

    Directory Introduction of Hashes

--- directory.rb
+++ directory.rb
@@ -1,15 +1,18 @@
 students = [
-    "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"]
+    {name: "Dr. Hannibal Lecter", cohort: :november}, 
+    {name: "Darth Vader", cohort: :november},
+    {name: "Nurse Ratched", chort: :november}, 
+    {name: "Michael Corleone", cohort: :november},
+    {name: "Alex Delarge", cohort: :november}, 
+    {name: "The Wicked Witch of the West", cohort: :november},
+    {name: "Terminator", cohort: :november},
+    {name: "Freddy Krueger", cohort: :november},
+    {name: "The Joker", cohort: :november},
+    {name: "Joffrey Baratheon", cohort: :november},
+    {name: "Norman Bates", cohort: :november}
+    ]
+
+
 # and then print them
 def print_header
   puts "The students of Villains Academy"
@@ -17,7 +20,7 @@ def print_header
 end
 def print(names)
   names.each do |name|
-    puts name
+    puts "#{name[:name]} (#{name[:cohort]} cohort"
   end
 end
 def print_footer(names)
