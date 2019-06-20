ed949cfdf61abe0562ee518a0fa7b6e617f7158b
Charles Kettell <charleskettell@hotmail.co.uk>
2019-06-20 10:10:54 +0000

    Save Students

--- directory_play.rb
+++ directory_play.rb
@@ -92,7 +92,20 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to student.csv"
   puts "9. Exit"
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of studnets
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+  end
+  file.close
 end
 
 def show_students
@@ -107,6 +120,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else 
